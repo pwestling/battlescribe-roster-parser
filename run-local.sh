9d8d289ab611b767a674cb42985d0147f5dfad5d
Porter Westling <pwestling@liveramp.com>
2019-07-15 16:30:10 -0700

    improve local iter speed

--- run-local.sh
+++ run-local.sh
@@ -5,18 +5,8 @@ set -euo pipefail
 
 PORT=${1:-"8080"}
 
-stack build
-stack install
+stack build --no-docker
+stack install --no-docker
 
-docker network create tts 2>/dev/null || true
-
-docker build -t battlescribe-roster-parser:local .
-
-docker kill roster-parser-local 2>/dev/null || true
-docker rm roster-parser-local 2>/dev/null || true
-
-docker run -d --name parser-redis-local --network tts redis:latest 2>/dev/null || true
-docker start parser-redis-local 2>/dev/null || true
-container=$(docker run --network tts --name roster-parser-local -d -p 8080:$PORT -e REDIS_HOST=parser-redis-local battlescribe-roster-parser:local)
-echo "Local container is $container"
-docker logs -f $container
+docker run -d --name parser-redis-local -p6379:6379 redis:latest 2>/dev/null || true
+stack --no-docker exec battlescribe-roster-parser
