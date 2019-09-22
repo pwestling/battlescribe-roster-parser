87e3a209b2b1f92b6ad070c9e36054aa7da83d92
Porter Westling <pwestling@gmail.com>
2019-09-22 11:58:17 -0700

    always checkout master when done

--- testcommits.sh
+++ testcommits.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-CURRENT=$(git rev-parse --short HEAD)
+ONE=$(git rev-parse --short $1)
+TWO=$(git rev-parse --short $2)
+
 generate(){
   git checkout $1
-  OUTDIR="test_rosters/$(git rev-parse --short $1)"
+  stack build
+  OUTDIR="test_rosters/$2"
   mkdir -p "$OUTDIR"
   for f in $(ls test_rosters/*.testros) 
   do
     echo "Processing $f"
     stack exec battlescribe-roster-parser-cli $f 2>/dev/null | tail -n 1 | jq . > $OUTDIR/$(basename $f) 
   done  
-  git checkout $CURRENT
 }
 
-generate $1
-generate $2
+generate $ONE one
+generate $TWO two
+git checkout master
+diff test_rosters/one/ test_rosters/two/
