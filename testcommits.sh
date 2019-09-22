0d1cf862f1c4eaa73ed4ac9f9dbe1ea2a7bd0d09
Porter Westling <pwestling@gmail.com>
2019-09-22 11:46:28 -0700

    log processed files

--- testcommits.sh
+++ testcommits.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 CURRENT=$(git rev-parse --short HEAD)
-git add -A
-git stash
 generate(){
-  pushd test_rosters
   git checkout $1
-  OUTDIR=$(git rev-parse --short $1)
-  mkdir $OUTDIR
-  stack build
-  for f in $(ls *.testros) 
+  OUTDIR="test_rosters/$(git rev-parse --short $1)"
+  mkdir -p "$OUTDIR"
+  for f in $(ls test_rosters/*.testros) 
   do
-    stack exec battlescribe-roster-parser-cli $f 2>/dev/null | tail -n 1 | jq . > $OUTDIR/${f}.test 
+    echo "Processing $f"
+    stack exec battlescribe-roster-parser-cli $f 2>/dev/null | tail -n 1 | jq . > $OUTDIR/$(basename $f) 
   done  
-  popd
   git checkout $CURRENT
 }
 
 generate $1
 generate $2
-
-git stash apply
