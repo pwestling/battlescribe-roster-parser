04f6bbe695904d3d0ec67caa426926d2f20e5287
Porter Westling <pwestling@gmail.com>
2019-09-22 11:36:35 -0700

    change up script

--- testcommits.sh
+++ testcommits.sh
@@ -3,15 +3,22 @@
 set -euo pipefail
 
 CURRENT=$(git rev-parse --short HEAD)
+git add -A
+git stash
 generate(){
   pushd test_rosters
   git checkout $1
   OUTDIR=$(git rev-parse --short $1)
   mkdir $OUTDIR
-  for f in $(ls *.testros); do;
+  for f in $(ls *.testros) 
+  do
     stack exec battlescribe-roster-parser-cli $f 2>/dev/null | tail -n 1 | jq . > $OUTDIR/$f 
   done  
+  popd
+  git checkout $CURRENT
 }
 
 generate $1
 generate $2
+
+git stash apply
