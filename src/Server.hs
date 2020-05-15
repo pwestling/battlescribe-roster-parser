75bb16ff083fb14b622abf850c2e93b126ede0cf
Porter Westling <pwestling@gmail.com>
2020-05-15 13:06:00 -0700

    bump version

--- src/Server.hs
+++ src/Server.hs
@@ -42,7 +42,7 @@ import           Types
 
 
 version :: Version
-version = Version "1.7"
+version = Version "1.8"
 
 type CreateRosterAPI = "roster"
                        :> QueryFlag "addScripts" :> QueryParam "uiWidth" Int :> QueryParam "uiHeight" Int
